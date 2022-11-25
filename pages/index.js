676725313b8c0ec453f806dcd248661c7dfc3d9b
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-11-25 08:11:08 +0700

    clearing my code

--- pages/index.js
+++ pages/index.js
@@ -12,59 +12,6 @@ export default function Home() {
   const [dataProduct, setDataProduct] = useState(null)
   const [isLoading, setLoading] = useState(false)
   const [isCollapse, setCollapse] = useState(false)
-  
-  const formik = useFormik({
-    initialValues:{
-      name: '',
-      quantity: '',
-      price: '',
-      description: '',
-    },
-    // validate: register_validate,
-    onSubmit
-  })
-
-  async function onSubmit(values){
-
-    // setLoading(true);
-    const endpoint = 'http://localhost:8080/product';
-
-    const jData = JSON.stringify(values)
-
-    // console.log(jData)
-
-    // Form the request for sending data to the server.
-    const options = {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${document.cookie.split('; ').find((row) => row.startsWith('token='))?.split('=')[1]}`
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: jData // body data type must match "Content-Type"
-    }
-
-    // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options);
-
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-    const result = await response.json();
-    if(result){
-      if(result.message !== 'success'){
-        alert('something wrong')
-      } else {
-        alert(`input product ${result.message}`)
-        handleProductFetch()
-      }
-    }
-    // alert(`user with following id: ${result.insertedId} has inserted!`)
-  }
 
   useEffect(() => {
     // setLoading(true)
@@ -110,12 +57,155 @@ export default function Home() {
   })
 
   if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No data</p>
-  if (!dataProduct) return <p>No data</p>
+  if (!data) return <p>Loading...</p>
 
-  // async function handleClickData() {
-  //   console.log(data)
-  // }
+  function InputCollapse() {
+    const formik = useFormik({
+      initialValues:{
+        name: '',
+        quantity: '',
+        price: '',
+        description: '',
+      },
+      // validate: register_validate,
+      onSubmit
+    })
+
+    async function onSubmit(values){
+      const endpoint = 'http://localhost:8080/product';
+  
+      const jData = JSON.stringify(values)
+  
+      // Form the request for sending data to the server.
+      const options = {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${document.cookie.split('; ').find((row) => row.startsWith('token='))?.split('=')[1]}`
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: jData // body data type must match "Content-Type"
+      }
+  
+      // Send the form data to our forms API on Vercel and get a response.
+      const response = await fetch(endpoint, options);
+  
+      // Get the response data from server as JSON.
+      // If server returns the name submitted, that means the form works.
+      const result = await response.json();
+      if(result){
+        if(result.message !== 'success'){
+          alert('something wrong')
+        } else {
+          alert(`input product ${result.message}`)
+          handleProductFetch()
+        }
+      }
+      // alert(`user with following id: ${result.insertedId} has inserted!`)
+    }
+
+    return <>
+    {/* Input Product */}
+
+    <button className="btn btn-primary" onClick={() => setCollapse(!isCollapse)} type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Input
+    </button>
+    <br/>
+    <div className={isCollapse ? "" : "collapse"} id="collapseExample">
+      <div className="card card-body" style={{'marginTop': '20px', 'marginBottom': '30px'}}>
+      <form onSubmit={formik.handleSubmit}>
+        <div className='row'>
+          <div className='col'>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              id="exampleInputEmail1"
+              name='name'
+              {...formik.getFieldProps('name')}
+              />
+            </div>
+          </div>
+          <div className='col'>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Quantity</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              id="exampleInputPassword1"
+              name='quantity'
+              {...formik.getFieldProps('quantity')}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Price</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              id="exampleInputEmail1" 
+              name='price'
+              {...formik.getFieldProps('price')}
+              />
+            </div>
+          </div>
+          <div className='col'>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              id="exampleInputPassword1"
+              name='description'
+              {...formik.getFieldProps('description')}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='text-end' style={{'alignItems': 'end'}}>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+      </div>
+    </div>
+    </>
+  }
+
+  function ProductTable() {
+    if (!dataProduct) {
+      return <p>Loading</p>
+    } else {
+      return <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>{ dataProduct.map((f) => {
+          return<tr key={f._id}>
+            <td>{f.name}</td>
+            <td>{f.price}</td>
+            <td>{f.quantity}</td>
+            <td>{f.description}</td>
+          </tr>
+          })
+        }
+        </tbody>
+      </table>
+    }
+  } 
 
   async function handleProductFetch() {
     const endpoint = 'http://localhost:8080/product/';
@@ -152,95 +242,9 @@ export default function Home() {
         <div className="col-sm-8 text-start">
           <div className='card' style={{'marginTop': '30px', 'paddingTop': '20px'}}>
             <div className='container'>
-              <button class="btn btn-primary" onClick={() => setCollapse(!isCollapse)} type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Input
-              </button>
-              <br/>
-              <div class={isCollapse ? "" : "collapse"} id="collapseExample">
-                <div class="card card-body" style={{'marginTop': '20px', 'marginBottom': '30px'}}>
-                <form onSubmit={formik.handleSubmit}>
-                  <div className='row'>
-                    <div className='col'>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input 
-                        type="text" 
-                        class="form-control" 
-                        id="exampleInputEmail1"
-                        name='name'
-                        {...formik.getFieldProps('name')}
-                        />
-                      </div>
-                    </div>
-                    <div className='col'>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Quantity</label>
-                        <input 
-                        type="text" 
-                        class="form-control" 
-                        id="exampleInputPassword1"
-                        name='quantity'
-                        {...formik.getFieldProps('quantity')}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className='row'>
-                    <div className='col'>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Price</label>
-                        <input 
-                        type="text" 
-                        class="form-control" 
-                        id="exampleInputEmail1" 
-                        name='price'
-                        {...formik.getFieldProps('price')}
-                        />
-                      </div>
-                    </div>
-                    <div className='col'>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Description</label>
-                        <input 
-                        type="text" 
-                        class="form-control" 
-                        id="exampleInputPassword1"
-                        name='description'
-                        {...formik.getFieldProps('description')}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className='text-end' style={{'alignItems': 'end'}}>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </form>
-                </div>
-              </div>
-
+              <InputCollapse/>
               <hr/>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    dataProduct.map((f) => {
-                      return<tr>
-                        <td key={f._id}>{f.name}</td>
-                        <td>{f.price}</td>
-                        <td>{f.quantity}</td>
-                        <td>{f.description}</td>
-                      </tr>
-                    })
-                  }
-                </tbody>
-              </table>
+              <ProductTable/>
             </div>
           </div>
         </div>
